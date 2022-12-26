bead1c7e439402a9512a9f4c44d9187d1c4caaba
Nathan JANCZEWSKI <nathan@janczewski.fr>
2022-12-26 14:22:49 +0100

    [Ansible] Filtering on correct URL

---
+++ ansible/library/get_release_url.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+from ansible.module_utils.basic import *
+import requests
+import platform
+
+def main():
+
+    fields = {
+        'repository': { "required": True, "type": "str" }
+    }
+
+    module = AnsibleModule(argument_spec=fields)
+
+    arch = platform.uname()[4]
+    if arch == 'x86_64':
+        arch = 'amd64'
+
+    resp = requests.get(f'https://api.github.com/repos/{module.params["repository"]}/releases/latest')
+    
+    if resp.status_code != 200:
+        module.fail_json(changed=False, msg=f'Failed to get the Github releases: {resp.status_code}')
+
+    data = resp.json()
+
+    for asset in data['assets']:
+        if asset['name'].endswith(arch):
+            module.exit_json(changed=False, meta=asset['browser_download_url'])
+
+    module.fail_json(changed=False, msg='Binary not available for this architecture')
+
+if __name__ == '__main__':
+    main()
